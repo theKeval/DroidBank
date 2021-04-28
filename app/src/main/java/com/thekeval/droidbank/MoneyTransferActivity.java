70fe39f9905d53b1fc0e77123d03f87911348726
Keval <keval.langalia@live.com>
2021-04-27 20:55:58 -0400

    adding screens

---
+++ app/src/main/java/com/thekeval/droidbank/MoneyTransferActivity.java
@@ -0,0 +1,14 @@
+package com.thekeval.droidbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoneyTransferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_money_transfer);
+    }
+}
